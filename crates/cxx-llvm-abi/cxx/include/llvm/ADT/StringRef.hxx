9fb920a6b418f26a449c5e705c701795f35ef566
silvanshade <silvanshade@users.noreply.github.com>
2023-07-26 17:15:03 -0600

    Use CXX_MEMORY_ABI_PRELUDE macro

--- crates/cxx-llvm-abi/cxx/include/llvm/ADT/StringRef.hxx
+++ crates/cxx-llvm-abi/cxx/include/llvm/ADT/StringRef.hxx
@@ -6,111 +6,13 @@
 #include "llvm/ADT/StringRef.h"
 
 namespace cxx_llvm::llvm::adt::string_ref {
-using StringRef = ::llvm::StringRef;
-using F = StringRef;
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_abi_align() noexcept -> size_t
-{
-  return cxx_memory::abi::cxx_abi_align<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_abi_size() noexcept -> size_t
-{
-  return cxx_memory::abi::cxx_abi_size<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_default_constructible() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_default_constructible<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_copy_constructible() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_copy_constructible<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_move_constructible() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_move_constructible<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_destructible() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_destructible<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_trivially_copyable() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_trivially_copyable<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_trivially_movable() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_trivially_movable<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_trivially_destructible() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_trivially_destructible<F>();
-}
-
-} // namespace cxx_llvm::llvm::adt::string_ref
-
-namespace cxx_llvm::llvm::adt::string_ref {
-[[gnu::always_inline]]
-static inline auto
-cxx_default_new(F* This [[clang::lifetimebound]]) noexcept -> void
-{
-  return cxx_memory::abi::cxx_default_new(This);
-}
-
-[[gnu::always_inline]]
-static inline auto
-cxx_copy_new(F* This [[clang::lifetimebound]], F const& that [[clang::lifetimebound]]) noexcept -> void
-{
-  return cxx_memory::abi::cxx_copy_new(This, that);
-}
-
-[[gnu::always_inline]]
-static inline auto
-cxx_move_new(F* This [[clang::lifetimebound]], F* that [[clang::lifetimebound]]) noexcept -> void
-{
-  // NOLINTNEXTLINE(hicpp-move-const-arg, performance-move-const-arg)
-  F&& that_rvalue = ::std::move(*that);
-  return cxx_memory::abi::cxx_move_new(This, ::std::forward<F&&>(that_rvalue));
-}
-
-[[gnu::always_inline]]
-static inline auto
-cxx_destruct(F* This [[clang::lifetimebound]]) -> void
-{
-  return cxx_memory::abi::cxx_destruct(This);
-}
-
+CXX_MEMORY_ABI_PRELUDE(StringRef, ::llvm::StringRef)
 } // namespace cxx_llvm::llvm::adt::string_ref
 
 namespace cxx_llvm::llvm::adt::string_ref {
 [[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
 static inline auto
-new_from_rust_str(rust::Str str [[clang::lifetimebound]]) noexcept -> F
+new_from_rust_str(rust::Str str [[clang::lifetimebound]]) noexcept -> Type
 {
   std::string_view&& view = { str.data(), str.size() };
   return { view };
@@ -118,14 +20,14 @@ new_from_rust_str(rust::Str str [[clang::lifetimebound]]) noexcept -> F
 
 [[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
 static inline auto
-new_from_rust_slice(rust::Slice<char const> const slice [[clang::lifetimebound]]) noexcept -> F
+new_from_rust_slice(rust::Slice<char const> const slice [[clang::lifetimebound]]) noexcept -> Type
 {
   return { slice.data(), slice.size() };
 }
 
 [[nodiscard]] [[gnu::always_inline]]
 static inline auto
-as_slice(F This [[clang::lifetimebound]]) noexcept -> rust::Slice<char const>
+as_slice(Type This [[clang::lifetimebound]]) noexcept -> rust::Slice<char const>
 {
   return { This.data(), This.size() };
 }
--- crates/cxx-llvm-abi/cxx/include/llvm/ADT/Triple.hxx
+++ crates/cxx-llvm-abi/cxx/include/llvm/ADT/Triple.hxx
@@ -13,112 +13,14 @@ using TripleEnvironmentType = ::llvm::Triple::EnvironmentType;
 using TripleObjectFormatType = ::llvm::Triple::ObjectFormatType;
 
 namespace cxx_llvm::llvm::adt::triple {
-using Triple = ::llvm::Triple;
-using F = Triple;
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_abi_align() noexcept -> size_t
-{
-  return cxx_memory::abi::cxx_abi_align<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_abi_size() noexcept -> size_t
-{
-  return cxx_memory::abi::cxx_abi_size<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_default_constructible() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_default_constructible<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_copy_constructible() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_copy_constructible<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_move_constructible() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_move_constructible<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_destructible() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_destructible<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_trivially_copyable() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_trivially_copyable<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_trivially_movable() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_trivially_movable<F>();
-}
-
-[[nodiscard]] [[gnu::always_inline]] [[gnu::const]]
-constexpr static inline auto
-cxx_is_trivially_destructible() noexcept -> bool
-{
-  return cxx_memory::abi::cxx_is_trivially_destructible<F>();
-}
-
-} // namespace cxx_llvm::llvm::adt::triple
-
-namespace cxx_llvm::llvm::adt::triple {
-[[gnu::always_inline]]
-static inline auto
-cxx_default_new(F* This [[clang::lifetimebound]]) noexcept -> void
-{
-  return cxx_memory::abi::cxx_default_new(This);
-}
-
-[[gnu::always_inline]]
-static inline auto
-cxx_copy_new(F* This [[clang::lifetimebound]], F const& that [[clang::lifetimebound]]) noexcept -> void
-{
-  return cxx_memory::abi::cxx_copy_new(This, that);
-}
-
-[[gnu::always_inline]]
-static inline auto
-cxx_move_new(F* This [[clang::lifetimebound]], F* that [[clang::lifetimebound]]) noexcept -> void
-{
-  // NOLINTNEXTLINE(hicpp-move-const-arg, performance-move-const-arg)
-  F&& that_rvalue = ::std::move(*that);
-  return cxx_memory::abi::cxx_move_new(This, ::std::forward<F&&>(that_rvalue));
-}
-
-[[gnu::always_inline]]
-static inline auto
-cxx_destruct(F* This [[clang::lifetimebound]]) -> void
-{
-  return cxx_memory::abi::cxx_destruct(This);
-}
-
+CXX_MEMORY_ABI_PRELUDE(Triple, ::llvm::Triple)
 } // namespace cxx_llvm::llvm::adt::triple
 
 namespace cxx_llvm::llvm::adt::triple {
 [[gnu::always_inline]] [[gnu::const]]
 static inline auto
 placement_new_from_arch_vendor_os(
-  F* This,
+  Type* This,
   ::llvm::Twine const& arch,
   ::llvm::Twine const& vendor,
   ::llvm::Twine const& os
@@ -135,7 +37,7 @@ placement_new_from_arch_vendor_os(
 [[gnu::always_inline]] [[gnu::const]]
 static inline auto
 placement_new_from_arch_vendor_os_environment(
-  F* This,
+  Type* This,
   ::llvm::Twine const& arch,
   ::llvm::Twine const& vendor,
   ::llvm::Twine const& os,
