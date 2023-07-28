0e94f8c2ac2e6bd8d94293341de5122b5ed1d1e6
silvanshade <silvanshade@users.noreply.github.com>
2023-07-28 08:04:47 -0600

    Remove auto-generated files from git

--- crates/cxx-llvm-abi/src/abi.rs
+++ crates/cxx-llvm-abi/src/abi.rs
@@ -1,17 +1,6 @@
-#![doc = r" NOTE: This module is auto-generated and should not be edited."]
-pub mod llvm;
-pub(crate) fn write_module() -> ::cxx_memory_abi::BoxResult<()> {
-    let path_components = &[];
-    let path_descendants = &["llvm"];
-    ::cxx_memory_abi::CxxAbiArtifactInfo::write_module_for_dir(path_components, path_descendants)
-}
-pub fn process_artifacts() -> ::cxx_memory_abi::BoxResult<()> {
-    self::write_module()?;
-    self::llvm::write_module()?;
-    self::llvm::adt::write_module()?;
-    self::llvm::adt::hash_code::write_module()?;
-    self::llvm::adt::string_ref::write_module()?;
-    self::llvm::adt::triple::write_module()?;
-    self::llvm::adt::twine::write_module()?;
+type BoxError = Box<dyn std::error::Error + Send + Sync + 'static>;
+type BoxResult<T> = Result<T, BoxError>;
+
+pub fn process_artifacts() -> BoxResult<()> {
     Ok(())
 }
